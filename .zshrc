a185676505eb5767f248648932ef0c7bc478bc4b
Alex Ciobica <alex.ciobica@gmail.com>
2013-07-07 20:06:45 +0300

    Update env.

--- .zshrc
+++ .zshrc
@@ -2,18 +2,10 @@
 export DOTFILES=$HOME/bin/dotfiles
 
 # Source the environment variables
-. $DOTFILES/bash/env.bash
+. $DOTFILES/shell/env.bash
 
 # Source the oh-my-zsh config
-. $DOTFILES/bash/oh_my_zsh.bash
+. $DOTFILES/shell/oh_my_zsh.bash
 
 # Source my aliases and scripts
-. $DOTFILES/bash/c10b10.bash
-
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
-
-# Activate bash completion for WP-CLI
-autoload bashcompinit
-bashcompinit
-source ~/.composer/vendor/wp-cli/wp-cli/utils/wp-completion.zsh
+source_if_exists $DOTFILES/shell/c10b10.bash
--- shell/env.bash
+++ shell/env.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO Cleanup the mess
+
 source_if_exists() {
     if [ -f $1 ]
     then
@@ -16,7 +18,14 @@ export WP_CLI_PHP=/Applications/MAMP/bin/php/php5.4.4/bin/php
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
 
+export PATH="/Applications/MAMP/Library/bin:/usr/local/bin:$PATH"
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
+export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules"
+
 # Activate bash completion for WP-CLI
 autoload bashcompinit
 bashcompinit
 source_if_exists $HOME/.composer/vendor/wp-cli/wp-cli/utils/wp-completion.bash
+
