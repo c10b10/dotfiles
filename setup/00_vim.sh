c05b7e7eccf5fbe6e078af9e4e58cb996c96f926
Alex Ciobica <alex.ciobica@gmail.com>
2014-01-23 00:31:30 +0100

    Lint.

--- setup/00_vim.sh
+++ setup/00_vim.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo -e "There's nothin I can do except tell you what you can do."
 echo -e "1. Make sure you have run `git submodule update --init --recursive` from the dotfiles repo."
 echo -e "2. Launch vim and run :BundleInstall. Wait until vim has finished."
