da8806d090a9c77684f8a8d4edd73af95e36e766
Alex Ciobica <alex.ciobica@gmail.com>
2014-01-22 14:24:27 +0100

    Update the php setup script.

--- setup/01_php.sh
+++ setup/01_php.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 set -e
 
-brew tap josegonzalez/php
-brew tap homebrew/dupes
+echo_color()
+{
+    background=${2-'47'}
+    foreground=${3-'0;30'}
+    echo -e "\033[${foreground}m\033[${background}m$1\033[0m";
+}
 
-echo 'Updating homebrew'
-brew update
-brew tap --repair
+setup_php_brew() {
+    echo -e "Tapping kegs and updating brew..."
+    brew tap josegonzalez/php
+    brew tap homebrew/dupes
+    brew update
+    brew tap --repair
+    echo -e "Done!\n"
+}
 
-# Install PHP
-brew install php53 --with-mysql --with-debug
-brew install php54 --with-mysql --with-debug
+install_php() {
+    echo -e "Installing php..."
+    brew install php53 --with-mysql --with-debug
+    brew install php54 --with-mysql --with-debug
+    echo -e "Done!\n"
+}
+
+echo_color "Starting the php installation process..."; echo
+
+setup_php_brew
+install_php
+php-switch-osx
+
+echo -e "\nYou can switch php versions using 'php-switch-osx'."
+
+echo; echo_color 'All done!'
 
 # Install composer
 # https://github.com/composer/composer
