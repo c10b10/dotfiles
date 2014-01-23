8f50828976cf6eef486341a46c443c516c3ec488
Alex Ciobica <alex.ciobica@gmail.com>
2014-01-24 00:18:45 +0100

    Update the php extension install script.

--- setup/02_php_extensions.sh
+++ setup/02_php_extensions.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -e
 
-# See list: https://github.com/josegonzalez/homebrew-php/tree/master/Formula
+# Usage: ~/.dotfiles/setup/02_php_extensions.sh [php5x]
+# If the script argument is omitted, the PHP_EXTENSIONS wil be installed for the PHP_VERSIONS defined
+
+# See list of extensions available here:
+# https://github.com/josegonzalez/homebrew-php/tree/master/Formula
+
+set -e
+PHP_EXTENSIONS='xdebug mcrypt'
+PHP_VERSIONS=$(php-installed)
 
 echo_color()
 {
@@ -26,7 +33,7 @@ install_ext_for_ver() {
 
     echo; echo_color "Installing $PHP_VER extensions..."
 
-    IFS=',' read -ra EXT <<< "$EXT_LIST"
+    IFS=' ' read -ra EXT <<< "$EXT_LIST"
     local i; for i in "${EXT[@]}"; do
         echo -e "\nInstalling $i..."
         brew install $PHP_VER-$i
@@ -37,16 +44,33 @@ install_ext_for_ver() {
 # installs all php extensions for each php environment installed
 install_php_ext() {
     local EXT_LIST=$1
-    local PHP_CURR=$(php-current-version)
-    local i; IFS=' ' read -ra VER <<< "$(php-installed)"
+    local PHP_VERSIONS=$2
+    local i; IFS=' ' read -ra VER <<< "$PHP_VERSIONS"
     for i in "${VER[@]}"; do
         php-switch-osx $i > /dev/null
         install_ext_for_ver $EXT_LIST $i
     done
 }
 
+sanitize_php() {
+    local PHP=${1-''}
+    IFS=' ' read -ra ALL <<< "$(php-installed)"
+
+    # exit if valid version is supplied
+    if [[ ! -z $PHP ]]; then
+        for i in ${ALL[@]}; do
+            if [[ $i == $PHP ]]; then
+                echo $PHP && return
+            fi
+        done
+    fi
+
+    echo $PHP_VERSION
+}
+
 echo_color "Starting the php extensions installation process...";
-install_php_ext "xdebug,mcrypt"
+install_php_ext $PHP_EXTENSIONS $(sanitize_php $1)
+
 echo; echo_color 'All done!'
 
 echo -e "\nNOTE: Installing php extensions with homebrew adds files to:\n\t/usr/local/etc/php/%version%/conf.d/ext-%ext_name%.ini\nThese files *are not* removed by homebrew at uninstall, i.e. with 'brew remove %ext_name%'."
