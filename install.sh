e57b8897e48d1d4741123c665d2180bddd154a69
Alex Ciobica <alex.ciobica@gmail.com>
2015-04-20 16:12:49 +0300

    Add install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -fs $C10_DOTFILES/ackrc $HOME/.ackrc
+ln -fs $C10_DOTFILES/agignore $HOME/.agignore
+ln -fs $C10_DOTFILES/vimrc $HOME/.vimrc
+ln -fs $C10_DOTFILES/vim/ $HOME/.vim
+ln -fs $C10_DOTFILES/zshrc $HOME/.zshrc
+ln -fs $C10_DOTFILES/npmrc $HOME/.npmrc
+ln -fs $C10_DOTFILES/gitignore $HOME/.gitignore
