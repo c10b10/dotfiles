44042fae7c8dd6139ec810c45b6c496a43b97cc3
root <alex.ciobica@gmail.com>
2015-04-21 06:41:18 -0400

    Update install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -fs $C10_DOTFILES/ackrc $HOME/.ackrc
-ln -fs $C10_DOTFILES/agignore $HOME/.agignore
-ln -fs $C10_DOTFILES/vimrc $HOME/.vimrc
-ln -fs $C10_DOTFILES/vim/ $HOME/.vim
-ln -fs $C10_DOTFILES/zshrc $HOME/.zshrc
-ln -fs $C10_DOTFILES/npmrc $HOME/.npmrc
-ln -fs $C10_DOTFILES/gitignore $HOME/.gitignore
+. $HOME/.c10
+
+rm -rf $HOME/.vim
+
+readarray <<HERE
+ackrc
+agignor
+vimrc
+vim
+zshrc
+npmrc
+gitignore
+HERE
+
+for file in "${MAPFILE[@]}"; do
+  # remove trailing new line
+  cleanfile=$(echo -n $file)
+  ln -fs $C10_DOTFILES/$cleanfile $HOME/.$cleanfile
+done
