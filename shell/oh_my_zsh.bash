4f750aa61d31e6e7ddebce39b4bf9809d9da0ea3
Alex Ciobica <alex.ciobica@gmail.com>
2015-04-21 17:50:43 +0300

    Use server theme.

--- shell/oh_my_zsh.bash
+++ shell/oh_my_zsh.bash
@@ -10,7 +10,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 # ZSH_THEME="robbyrussell"
-ZSH_THEME="c10"
+ZSH_THEME="c10-srv"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
