75b65eed32bccc1948a9b0313527b71ca841d671
Alex Ciobica <alex.ciobica@gmail.com>
2015-12-01 14:52:05 +0200

    Update install script.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+C10_DOTFILES={$1:-$C10_DOTFILES}
 
 symlinks=( \
     'ackrc' \
@@ -21,5 +22,6 @@ for file in "${symlinks[@]}"; do
   # http://stackoverflow.com/questions/9532654/bash-expression-after-last-specific-character
   echo "Linking $file to $HOME/.${file##*/}"
   rm -rf $HOME/.$file
+  echo "ln -fs $C10_DOTFILES/$file $HOME/.${file##*/}"
   ln -fs $C10_DOTFILES/$file $HOME/.${file##*/}
 done
